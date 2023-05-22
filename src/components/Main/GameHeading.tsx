819bb0cd134d672c576782a52c29226eae12ee2f
Harriet Ayugi <clerryha@gmail.com>
2023-05-22 21:59:26 +0300

    customise theme for darker gray

--- src/components/Main/GameHeading.tsx
+++ src/components/Main/GameHeading.tsx
@@ -3,7 +3,7 @@ import { GameQuery } from "../../App";
 
 const GameHeading = ({ gameQuery }: { gameQuery: GameQuery }) => {
   const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name
+    gameQuery.genre?.name || ""
   } Games`;
   return (
     <Heading as="h1" fontSize="5xl">
