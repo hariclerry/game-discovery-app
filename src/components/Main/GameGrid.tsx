057af36b4aae665b911aaa7037d9f4614add7b81
Harriet Ayugi <clerryha@gmail.com>
2023-05-22 22:01:52 +0300

    fix error display for games

--- src/components/Main/GameGrid.tsx
+++ src/components/Main/GameGrid.tsx
@@ -1,5 +1,4 @@
-import { Heading, HStack, SimpleGrid, Text } from "@chakra-ui/react";
-import { ChatIcon } from "@chakra-ui/icons";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import Cards from "./Cards";
 import useGames from "../../hooks/useGames";
 import CardSkeleton from "./CardSkeleton";
@@ -13,29 +12,27 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, isLoading, error } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return null;
+  if (error) return <Text>{error}</Text>;
 
   return (
-    <>
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 2, xl: 3, "2xl": 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <CardContainer key={skeleton}>
-              <CardSkeleton />
-            </CardContainer>
-          ))}
-        {data.map((game, index) => (
-          <CardContainer key={game.id}>
-            {" "}
-            <Cards game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 2, xl: 3, "2xl": 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <CardContainer key={skeleton}>
+            <CardSkeleton />
           </CardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game, index) => (
+        <CardContainer key={game.id}>
+          {" "}
+          <Cards game={game} />
+        </CardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
